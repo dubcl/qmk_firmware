c17f2137f4f837f9bd87a287fb8055e68945a230
Carlos Albornoz <carlos@albornoz.rocks>
2021-03-21 11:13:02 -0300

    disable rgb, add home insert

--- keyboards/crkbd/keymaps/dubcl/keymap.c
+++ keyboards/crkbd/keymaps/dubcl/keymap.c
@@ -71,9 +71,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|-----------------------------------------------------|                    |-----------------------------------------------------|
       KC_ESC ,  KC_1 ,  KC_2 ,  KC_3 ,  KC_4 ,  KC_5 ,                            KC_6 ,  KC_7 ,  KC_8 ,  KC_9 ,  KC_0 ,  KC_DEL , \
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT ,  KC_NO ,  KC_NO ,  KC_NO ,  KC_NO ,  KC_NO ,                      KC_NO ,  KC_NO ,  KC_NO ,  KC_UP ,  KC_NO ,  KC_PGUP , \
+      KC_LSFT ,  KC_NO ,  KC_NO ,  KC_NO ,  KC_NO ,  KC_NO ,                      KC_HOME ,  KC_NO ,  KC_NO ,  KC_UP ,  KC_NO ,  KC_PGUP , \
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL ,  KC_NO ,  KC_NO ,  KC_NO ,  KC_NO ,  KC_NO ,                      KC_NO ,  KC_NO ,  KC_LEFT ,  KC_DOWN ,  KC_RGHT ,  KC_PGDN , \
+      KC_LCTL ,  KC_NO ,  KC_NO ,  KC_NO ,  KC_NO ,  KC_NO ,                      KC_END ,  KC_NO ,  KC_LEFT ,  KC_DOWN ,  KC_RGHT ,  KC_PGDN , \
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                          KC_LGUI, KC_TRNS, KC_SPC,    KC_SPC, RAISE, KC_RALT
                                       //|--------------------------|  |--------------------------|
